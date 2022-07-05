dd71927fc8d81f9f129bfa9cd610d81d661f363e
João Godoi <jpsgodoi@gmail.com>
2022-07-05 10:25:46 +0100

    Initial commit

---
+++ tests/features/demo.feature
@@ -0,0 +1,13 @@
+Feature: Demo feature
+
+    @Demo
+    Scenario Outline: Demo test
+
+        Given google page is opened
+        When the user searches with <SearchItem>
+        And the user clicks the first search result
+        Then the URL should match <ExpectedURL>
+
+        Examples:
+            | TestCase | SearchItem | ExpectedURL           |
+            | JIRA-01  | WDIO       | https://webdriver.io/ |
---
+++ tests/steps-definitions/demo-steps.ts
@@ -0,0 +1,28 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import chai from "chai";
+
+Given(/^google page is opened$/, async function () {
+    await browser.url("https://www.google.com");
+    let acceptCookie = await $('(//button)[4]');
+    await acceptCookie.click();
+    await browser.pause(7000);
+});
+
+When(/^the user searches with (.*)$/, async function (searchItem: string) {
+    console.log(`Searching with --> ${searchItem}`);
+    let searchBox = await $("input[type='text']");
+    await searchBox.setValue(searchItem);
+    await browser.keys("Enter");
+});
+
+When(/^the user clicks the first search result$/, async function () {
+    console.log("Clicking in the first result");
+    let searchResult = await $("(//cite[@role='text'])[1]");
+    await searchResult.click();
+});
+
+Then(/^the URL should match (.*)$/, async function (expectedUrl: string) {
+    console.log(`Expected URL --> ${expectedUrl}`);
+    let actualUrl = await browser.getUrl();
+    chai.expect(actualUrl).to.equal(expectedUrl);
+});

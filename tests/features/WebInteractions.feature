2f8039c0b2b85c1f33354142e7689fb7eba2f2d1
João Godoi <jpsgodoi@gmail.com>
2022-07-07 18:52:47 +0100

    Added additional web interactions

--- tests/features/WebInteractions.feature
+++ tests/features/WebInteractions.feature
@@ -5,5 +5,8 @@ Feature: Web interactions
 
         Given the web interactions page is opened
         # When the user performs input web interactions
-        And the user performs dropdown web interactions
-
+        # And the user performs dropdown web interactions
+        # And the user performs checkbox web interactions
+        # And the user performs windows handling web interactions
+        # And the user performs handling alerts web interactions
+        # And the user performs file upload web interactions
--- tests/steps-definitions/web-interactions-steps.ts
+++ tests/steps-definitions/web-interactions-steps.ts
@@ -62,3 +62,91 @@ When(/^the user performs dropdown web interactions$/, async function () {
     let expectedOptions = ['Please select an option', 'Option 1', 'Option 2'];
     chai.expect(arr).to.have.all.members(expectedOptions);
 });
+
+When(/^the user performs checkbox web interactions$/, async function () {
+    /**
+     * 3. Checkbox
+     */
+    // navigate to the interactions page
+    await driver.url(await browser.getUrl() + "checkboxes");
+
+    // if not selected, go and select it
+    let checkboxOption1 = await $("(//input[@type='checkbox'])[1]");
+
+    if (!await checkboxOption1.isSelected()) {
+        await checkboxOption1.click();
+    }
+
+    await browser.pause(3000)
+});
+
+When(/^the user performs windows handling web interactions$/, async function () {
+    /**
+     * 4. Windows handling
+     */
+    // navigate to the interactions page
+    await driver.url(await browser.getUrl() + "windows");
+
+    let parentWindow = await browser.getWindowHandle();
+
+    await $("a[href='/windows/new']").click();
+    await $("//a[text()='Elemental Selenium']").click();
+
+    // switch to specific window
+    let winHandles = await browser.getWindowHandles();
+    for (const element of winHandles) {
+        console.log(`>>> win handle: ${element}`);
+        await browser.switchToWindow(element);
+
+        let winTitle = await browser.getTitle();
+        console.log(`>>> current window title is: ${winTitle}`)
+
+    }
+
+    // switch to parent window
+    await browser.switchToWindow(parentWindow);
+    let winEle = await $("h3").getText();
+    chai.expect(winEle).to.equal("Opening a new window");
+
+    await browser.pause(3000)
+});
+
+When(/^the user performs handling alerts web interactions$/, async function () {
+    /**
+     * 5. Handling alerts
+     */
+    // navigate to the interactions page
+    await driver.url(await browser.getUrl() + "javascript_alerts");
+
+    await $(`button=Click for JS Alert`).click();
+    if (await browser.isAlertOpen()) {
+        await browser.pause(2000);
+        await browser.acceptAlert();
+    }
+
+    await $(`button=Click for JS Confirm`).click();
+    if (await browser.isAlertOpen()) {
+        let val = await browser.getAlertText();
+        console.log(`>>> this is the alert message: ${val}`);
+        await browser.pause(2000);
+        await browser.dismissAlert();
+    }
+    
+    await $(`button=Click for JS Prompt`).click();
+    if (await browser.isAlertOpen()) {
+        await browser.sendAlertText("Webdriver IO testing");
+        await browser.acceptAlert();
+        await browser.pause(2000)
+    }
+});
+
+When(/^the user performs file upload web interactions$/, async function () {
+    /**
+     * 6. File upload
+     */
+    // navigate to the interactions page
+    await driver.url(await browser.getUrl() + "");
+
+
+
+});
